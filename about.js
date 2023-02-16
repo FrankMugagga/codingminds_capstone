203ea4bff5ca326d0a6877f6173cf8a0eb7721ff
FrankMugagga <frank.mugagga@gmail.com>
2023-02-16 18:04:39 +0300

    Corrected eslint and style lint

--- about.js
+++ about.js
@@ -3,11 +3,11 @@ const menu = document.getElementById('get_menu');
 const close = document.getElementById('close');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('menu-show');
-    menu.classList.toggle('menu-hide');    
+  menu.classList.toggle('menu-show');
+  menu.classList.toggle('menu-hide');
 });
 
 close.addEventListener('click', () => {
-    menu.classList.toggle('menu-show');
-    menu.classList.toggle('menu-hide');    
+  menu.classList.toggle('menu-show');
+  menu.classList.toggle('menu-hide');
 });
--- index.js
+++ index.js
@@ -1,73 +1,65 @@
-
 const hamburger = document.getElementsByClassName('hamb')[0];
-const menu = document.getElementById('get_menu');     
+const menu = document.getElementById('get_menu');
 const closeButton = document.getElementById('close');
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('menu-show');
-    menu.classList.toggle('menu-hide');
-})
-
-closeButton.addEventListener('click', () => {
-    menu.classList.toggle('menu-show');
-    menu.classList.toggle('menu-hide');
+  menu.classList.toggle('menu-show');
+  menu.classList.toggle('menu-hide');
 });
 
+closeButton.addEventListener('click', () => {
+  menu.classList.toggle('menu-show');
+  menu.classList.toggle('menu-hide');
+});
 
-
-
-let speakerSection = document.getElementById('wrapper');
+const speakerSection = document.getElementById('wrapper');
 
 const speakers = [
-    {        
-        image : 'images/speakers/Brendan_Eich.jpg',
-        speakerName : 'Brendan Eich',
-        speakerProfession : 'American computer programmer',
-        speakerDetail : 'Brendan Eich is an American computer programmer and technology executive.',
-    },
+  {
+    image: 'images/speakers/Brendan_Eich.jpg',
+    speakerName: 'Brendan Eich',
+    speakerProfession: 'American computer programmer',
+    speakerDetail: 'Brendan Eich is an American computer programmer and technology executive.',
+  },
 
-    {       
-        image : 'images/speakers/Dennis_Ritchie.jpg',
-        speakerName : 'Dennis Ritchie',
-        speakerProfession : 'American computer scientist',
-        speakerDetail : 'Dennis MacAlistair Ritchie was an American computer scientist.',
-    },
+  {
+    image: 'images/speakers/Dennis_Ritchie.jpg',
+    speakerName: 'Dennis Ritchie',
+    speakerProfession: 'American computer scientist',
+    speakerDetail: 'Dennis MacAlistair Ritchie was an American computer scientist.',
+  },
 
-    {       
-        image : 'images/speakers/ken_thompson.jpg',
-        speakerName : 'Ken Thompson',
-        speakerProfession : 'American computer designer',
-        speakerDetail : 'Kenneth Lane Thompson is an American pioneer of computer science.'
-    },
+  {
+    image: 'images/speakers/ken_thompson.jpg',
+    speakerName: 'Ken Thompson',
+    speakerProfession: 'American computer designer',
+    speakerDetail: 'Kenneth Lane Thompson is an American pioneer of computer science.',
+  },
 
-    {       
-        image : 'images/speakers/Larry_Page.jpg',
-        speakerName : 'Larry Page',
-        speakerProfession : 'American business magnate',
-        speakerDetail : 'Lawrence Edward Page is an American business magnate, computer scientist and internet entrepreneur.',
-    },
+  {
+    image: 'images/speakers/Larry_Page.jpg',
+    speakerName: 'Larry Page',
+    speakerProfession: 'American business magnate',
+    speakerDetail: 'Lawrence Edward Page is an American business magnate, computer scientist and internet entrepreneur.',
+  },
 
-    {       
-        image : 'images/speakers/Margaret_Hamilton.jpg',
-        speakerName : 'Margaret Hamilton',
-        speakerProfession : 'American computer scientist',
-        speakerDetail : 'Margaret Heafield Hamilton is an American computer scientist.',
-    },
+  {
+    image: 'images/speakers/Margaret_Hamilton.jpg',
+    speakerName: 'Margaret Hamilton',
+    speakerProfession: 'American computer scientist',
+    speakerDetail: 'Margaret Heafield Hamilton is an American computer scientist.',
+  },
 
-    {       
-        image : 'images/speakers/Radia-Perlman.jpg',
-        speakerName : 'Radia Perlman',
-        speakerProfession : 'American network engineer',
-        speakerDetail : 'Radia Joy Perlman is an American computer programmer and network engineer.',
-    }
+  {
+    image: 'images/speakers/Radia-Perlman.jpg',
+    speakerName: 'Radia Perlman',
+    speakerProfession: 'American network engineer',
+    speakerDetail: 'Radia Joy Perlman is an American computer programmer and network engineer.',
+  },
 ];
 
-
-let speakerContent = ``;
-speakers.forEach((item) => { 
-
-  speakerContent += ` 
-   
-        
+let speakerContent = '';
+speakers.forEach((item) => {
+  speakerContent += `          
         <li class="speaker_image_content">
             
             <div class="img_background">
@@ -86,9 +78,3 @@ speakers.forEach((item) => {
 });
 
 speakerSection.innerHTML = speakerContent;
-
-/*
-const tr = document.getElementById('tr');
-tr.addEventListener('click', () => {
-    alert(speakerContent);
-});*/
