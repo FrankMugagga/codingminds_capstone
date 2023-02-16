99ac1dbaf278f519951abd731b6ae2cb3c40fdc4
FrankMugagga <frank.mugagga@gmail.com>
2023-02-16 15:45:36 +0300

    Added the mobile menu to the and did styling

---
+++ about.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementsByClassName('hamb')[0];
+const menu = document.getElementById('get_menu');
+const close = document.getElementById('close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('menu-show');
+    menu.classList.toggle('menu-hide');    
+});
+
+close.addEventListener('click', () => {
+    menu.classList.toggle('menu-show');
+    menu.classList.toggle('menu-hide');    
+});
